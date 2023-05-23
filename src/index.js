a691f97bc1697a8860a6a9bcb16476a1492cc409
Ronny Rivera <ronny.b.riv@gmail.com>
2023-05-23 13:15:17 -0400

    Simple changes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM,render(<App>, document.querySelector("#root")</App>)
